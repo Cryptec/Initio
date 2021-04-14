8a52fef5c7501176f511e2a7871e85d9a99e621a
Cryptec <neupi92@web.de>
2021-04-14 22:44:51 +0200

    switched Logout component to sidebar

--- src/Components/Sidebar.js
+++ src/Components/Sidebar.js
@@ -1,9 +1,25 @@
 import React, { Component } from 'react';
 import { Link } from "react-router-dom";
+import { logout, isLogin } from '../utils'
 
 import '../css/Sidebar.css'
 
 class Sidebar extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            isLogin: isLogin()
+        }
+    }
+
+    handleLogout = () => {
+        logout();
+        this.props.history.push('/login')
+        this.setState({
+            isLogin: false
+        })
+    }
 
     render () {
         return(
@@ -15,6 +31,14 @@ class Sidebar extends Component {
                         </li>
                         <li>
                             <Link to="/new">Neuer Artikel</Link>
+                        </li>
+                        <li>
+
+                                {this.state.isLogin ?
+                                    <button onClick={() => this.handleLogout()}>Logout</button>
+                                    : <Link to="/login" onClick={() => this.handleLogout()}></Link>
+                                }
+                            
                         </li>
                      
                     </ul>
--- src/Screens/Home.js
+++ src/Screens/Home.js
@@ -1,6 +1,4 @@
 import React, { Component } from 'react'
-import { logout, isLogin } from '../utils'
-import { Link } from 'react-router-dom'
 
 import '../css/Global.css'
 
@@ -8,33 +6,18 @@ import Sidebar from '../Components/Sidebar'
 
 class Home extends Component {
 
-    constructor(props) {
-        super(props);
 
-        this.state = {
-            isLogin: isLogin()
-        }
-    }
-
-    handleLogout = () => {
-        logout();
-        this.props.history.push('/login')
-        this.setState({
-            isLogin: false
-        })
-    }
 
     render() {
         return (
             <div>
-                <header className="headerBar">
-
-                    {this.state.isLogin ?
-                        <button className="logoutButton" onClick={() => this.handleLogout()}>Logout</button>
-                        : <Link to="/login" onClick={() => this.handleLogout()}></Link>
-                    }
-                </header>
+            <div>
+                
                 <Sidebar />
+            </div>
+            <div className="container">
+                HOME
+            </div>
             </div>
 
 
