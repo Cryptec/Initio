81d26deb123ede4578aa4311e8e71cefbd57293e
Cryptec <neupi92@web.de>
2021-05-09 20:24:00 +0200

    bring structure to Backend, add error handling

---
+++ src/Backend/routes/auth.js
@@ -0,0 +1,76 @@
+const router = require('express').Router();
+const Joi = require('@hapi/joi');
+var db = require('../Database');
+var md5 = require('md5')
+
+//Validation
+const schema = {
+    regname: Joi.string().min(4).required(),
+    regemail: Joi.string().min(4).required().email(),
+    regpassword: Joi.string().min(4).required(),
+}
+
+router.post('/register', async (req, res) => {
+
+  // Validate
+  const {error} = Joi.validate(req.body, schema);
+
+  var data = {
+      regname: req.body.regname,
+      regemail: req.body.regemail,
+      regpassword : md5(req.body.regpassword)
+  }
+  var sql ='INSERT INTO Users (regname, regemail, regpassword) VALUES (?,?,?)'
+  var params =[data.regname, data.regemail, data.regpassword]
+  db.run(sql, params, function (err, result) {
+      if (error){
+          res.status(400).send(error.details[0].message);
+          return;
+      }
+      res.json({
+          "answer": "Success",
+      })
+      res.status(200)
+  });
+})
+
+router.get("/users", (req, res, next) => {
+    var sql = "select * from Users"
+    var params = []
+    db.all(sql, params, (err, rows) => {
+      if (err) {
+        res.status(400).json({ "error": err.message });
+        return;
+      }
+      res.status(200).json(rows);
+    });
+  });
+  
+  router.get("/users/:id", (req, res, next) => {
+    var sql = "select * from Users where id = ?"
+    var params = [req.params.id]
+    db.get(sql, params, (err, row) => {
+        if (err) {
+          res.status(400).json({"error":err.message});
+          return;
+        }
+        res.json({
+            "answer":"success",
+            "data":row
+        })
+      });
+  });
+
+  router.post('/login', (req, res) => {
+    
+    // Validate
+    const {error} = Joi.validate(req.body, schema);
+    if (error){
+        res.status(400).send(error.details[0].message);
+        return;
+    }
+
+  })
+
+
+module.exports = router;
---
+++ src/Backend/routes/items.js
@@ -0,0 +1,98 @@
+const router = require('express').Router();
+var db = require('../Database');
+
+router.post("/bestand/", (req, res, next) => {
+    var errors = []
+    if (!req.body.Teilenummer) {
+      errors.push("No Teilenummer specified");
+    }
+    if (!req.body.SKU) {
+      errors.push("No SKU specified");
+    }
+    if (!req.body.Hersteller) {
+      errors.push("No Hersteller specified");
+    }
+    if (!req.body.Preis) {
+      errors.push("No Preis specified");
+    }
+    if (!req.body.Beschreibung) {
+      errors.push("No Beschreibung specified");
+    }
+    if (errors.length) {
+      res.status(400).json({ "error": errors.join(",") });
+      return;
+    }
+    var data = {
+      Teilenummer: req.body.Teilenummer,
+      SKU: req.body.SKU,
+      Hersteller: req.body.Hersteller,
+      Preis: req.body.Preis,
+      Beschreibung: req.body.Beschreibung,
+    }
+    var sql = 'INSERT INTO Teilebestand (Teilenummer, SKU, Hersteller, Preis, Beschreibung) VALUES (?,?,?,?,?)'
+    var params = [data.Teilenummer, data.SKU, data.Hersteller, data.Preis, data.Beschreibung]
+    
+    db.run(sql, params, function (err, result) {
+      if (err) {
+        res.status(400).json({ "error": err.message })
+        return;
+      }
+      res.json({
+        "answer": "Success",
+      })
+    });
+  })
+
+  router.patch("/bestand/", (req, res, next) => {
+    var reqBody = req.body;
+    db.run(`UPDATE Teilebestand set Teilenummer = ?, SKU = ?, Hersteller = ?, Preis = ?, Beschreibung = ? WHERE Teilebestand_id = ?`,
+        [reqBody.Teilenummer, reqBody.SKU, reqBody.Hersteller, reqBody.Preis, reqBody.Beschreibung, reqBody.Teilebestand_id],
+        function (err, result) {
+            if (err) {
+                res.status(400).json({ "error": res.message })
+                return;
+            }
+            res.status(200).json({ updatedID: this.changes });
+        });
+  });
+  router.delete("/bestand/:id", (req, res, next) => {
+    db.run(`DELETE FROM Teilebestand WHERE id = ?`,
+        req.params.id,
+        function (err, result) {
+            if (err) {
+                res.status(400).json({ "error": res.message })
+                return;
+            }
+            res.status(200).json({ deletedID: this.changes })
+        });
+  });
+
+  router.get("/bestand", (req, res, next) => {
+    var sql = "select * from Teilebestand"
+    var params = []
+    db.all(sql, params, (err, rows) => {
+      if (err) {
+        res.status(400).json({ "error": err.message });
+        return;
+      }
+      res.status(200).json(rows);
+    });
+  });
+  
+  
+  router.get("/bestand/:id", (req, res, next) => {
+    var sql = "select * from Teilebestand where id = ?"
+    var params = [req.params.id]
+    db.get(sql, params, (err, row) => {
+      if (err) {
+        res.status(400).json({ "error": err.message });
+        return;
+      }
+      res.json({
+        "answer": "success",
+        "data": row
+      })
+    });
+  });
+
+module.exports = router;
